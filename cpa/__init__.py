55f5898fb863070d4b1ff01f176f5aff31bae6ec
Mohsen Naghipourfar <naghipourfar@berkeley.edu>
2022-02-10 13:51:33 -0800

    minor API changes

--- cpa/__init__.py
+++ cpa/__init__.py
@@ -1,7 +1,10 @@
 from ._model import CPA
-from ._utils import register_dataset
+from ._data import prepare_dataset
+from ._plotting import *
+from . import _metrics as metrics
 
-__all__ = [
-    "CPA",
-    "register_dataset",
-]
+# __all__ = [
+#     "CPA",
+#     "prepare_dataset",
+#     "plot_training_progress"
+# ]
