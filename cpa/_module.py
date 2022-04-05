fixed minor issue for low batch size in disentanglement score computation

cpa/_module.py
@@ -391,7 +391,7 @@ class CPAModule(BaseModuleClass):
         #                                     cv=min(5, len(np.unique(drug_names.ravel()))),
         #                                     n_jobs=-1).mean()
 
-        knn_basal = knn_purity(latent_basal, drug_names.ravel())
+        knn_basal = knn_purity(latent_basal, drug_names.ravel(), n_neighbors=min(drug_names.shape[0], 30))
 
         for covar, unique_covars in self.covars_encoder.items():
             if len(unique_covars) > 1:
@@ -406,7 +406,8 @@ class CPAModule(BaseModuleClass):
                 #                                      scoring=make_scorer(balanced_accuracy_score),
                 #                                      cv=min(5, len(np.unique(target_covars.ravel()))),
                 #                                      n_jobs=-1).mean()
-                knn_basal += knn_purity(latent_basal, target_covars.ravel())
+                knn_basal += knn_purity(latent_basal, target_covars.ravel(),
+                                        n_neighbors=min(target_covars.shape[0], 30))
 
         # classifier = LogisticRegression(solver="liblinear",
         #                                 multi_class="auto",
@@ -418,7 +419,7 @@ class CPAModule(BaseModuleClass):
         #                               scoring=make_scorer(balanced_accuracy_score),
         #                               cv=5,
         #                               n_jobs=-1).mean()
-        knn_after = knn_purity(latent, drug_names.ravel())
+        knn_after = knn_purity(latent, drug_names.ravel(), n_neighbors=min(drug_names.shape[0], 30))
 
         for covar, unique_covars in self.covars_encoder.items():
             if len(unique_covars) > 1:
@@ -433,7 +434,7 @@ class CPAModule(BaseModuleClass):
                 #                                scoring=make_scorer(balanced_accuracy_score),
                 #                                cv=min(5, len(np.unique(target_covars.ravel()))),
                 #                                n_jobs=-1).mean()
-                knn_after += knn_purity(latent, target_covars.ravel())
+                knn_after += knn_purity(latent, target_covars.ravel(), n_neighbors=min(target_covars.shape[0], 30))
 
         return knn_basal, knn_after
 