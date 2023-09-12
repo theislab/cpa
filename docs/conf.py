ff016ad9538acc1e6a885c998e091fcede3b3afd
Mohsen Naghipourfar <naghipourfar@berkeley.edu>
2023-09-12 00:18:05 -0700

    mocked packages updated

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['torch', 'torch.nn', 'torch.optim']  # add other torch sub-modules if needed
+MOCK_MODULES = ['torch', 'torch.nn', 'torch.optim', 'torch.utils', 'pytorch_lightning', 'pytorch_lightning.callbacks']  
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 import cpa  # noqa
